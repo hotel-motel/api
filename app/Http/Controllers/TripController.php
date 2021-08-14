35a5cb55f3ec0e3f235317b7b12252ea23a003da
Omid Reza <snip77.orh@gmail.com>
2021-08-14 09:22:54 +0430

    validate request and check some condition

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\passenger;
 use App\Models\Room;
 use App\Models\Trip;
 use Carbon\CarbonPeriod;
+use App\Models\passenger;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TripController extends Controller
 {
     public function store(Room $room, Request $request)
     {
-        //check param must be exist
+        Validator::make($request->all(), [
+            'start'=>'required|date',
+            'passengers'=>'required|array',
+            'end'=>'required|date|after:start',
+            'passengers.*.last_name'=>'required',
+            'passengers.*.first_name'=>'required',
+            'passengers.*.national_code'=>'required|size:10'
+        ])->validate();
+        if ($request->passenger->count()>$room->max_capacity){
+            return abort(403, 'passenger count is more than room capacity');
+        }
         $tripDays=CarbonPeriod::create($request->start, $request->end);
         $tripsInDate=$room->trips->filter(function ($trip) use ($request, $tripDays){
             foreach ($tripDays as $day){
@@ -22,7 +33,7 @@ class TripController extends Controller
             }
         });
         if ($tripsInDate->count()>0){
-            return abort(403, 'can not reserve');
+            return abort(403, 'reserved for this period');
         }
         $trip=Trip::create([
             'start'=>$request->start,
@@ -38,6 +49,6 @@ class TripController extends Controller
                 'trip_id'=>$trip->id
             ]);
         }
-        return $request;
+        return response()->noContent();
     }
 }
