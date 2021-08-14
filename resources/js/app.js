28f682b7789bf9f6c7202922b59e05ba2fc3f409
Omid Reza <snip77.orh@gmail.com>
2021-08-15 01:18:52 +0430

    load compoentn in vue components part inside of use Vue.component(...)

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,9 @@ require('alpinejs');
 import Vue from 'vue/dist/vue.js';
 import passengerForm from './components/passengerform.vue';
 
-Vue.component('passenger-form', passengerForm)
-
 new Vue({
     el:'#app',
     components:{
-
+        passengerForm
     }
 });
