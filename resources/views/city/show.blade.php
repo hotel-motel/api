5913700301397bdbd2f846e0f58870b6a2587f4f
Omid Reza <snip77.orh@gmail.com>
2021-08-17 17:10:25 +0430

    add header

--- resources/views/city/show.blade.php
+++ resources/views/city/show.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-
+        <h4 class="font-bold">
+            {{ $city->name }} hotels
+        </h4>
     </x-slot>
     <city-hotels city_name="{{ $city->name }}"></city-hotels>
 </x-app-layout>
