36034f132cd09d4c86a1d6321e24f12771e82589
Omid Reza <snip77.orh@gmail.com>
2021-08-17 17:03:20 +0430

    fix some styl of citi list page

--- resources/views/city/list.blade.php
+++ resources/views/city/list.blade.php
@@ -1,12 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
+        <h4 class="font-bold">
+            Cities List
+        </h4>
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="row">
+                <div class="row justify-around m-3">
                     @foreach($cities as $city)
-                        <div class="col-4 mt-2">
+                        <div class="col-4 mb-3">
                             <city name="{{ $city->name }}" image="{{ $city->image }}"></city>
                         </div>
                     @endforeach
