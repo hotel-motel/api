944a4d88be179579b5c9677cc1eb806027a5a9e7
Omid Reza <snip77.orh@gmail.com>
2021-08-18 14:41:12 +0430

    Fix header and content

--- resources/views/trip/payment-verify.blade.php
+++ resources/views/trip/payment-verify.blade.php
@@ -1,13 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
-        @if(isset($error))
-            <div class="alert alert-danger">
-                {{ $error }}
-            </div>
-        @else
-            <div class="alert alert-success">
-                Room reserved
-            </div>
-        @endif
+        <h4>
+            Payment Status:
+        </h4>
     </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if(isset($error))
+                <div class="alert alert-danger">
+                    {{ $error }}
+                </div>
+            @else
+                <div class="alert alert-success">
+                    Room reserved
+                </div>
+            @endif
+        </div>
+    </div>
 </x-app-layout>
