f743913472128b62dc0b1f94d87fe9ef87d0d7de
Omid Reza <snip77.orh@gmail.com>
2021-08-20 10:27:45 +0430

    Changelogs : 1. install vue router 2. create dashboard page and setup router for dashboard items TODO: - complete dashboard pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,22 @@ require('./bootstrap');
 
 require('alpinejs');
 
-import city from "./components/city";
 import Vue from 'vue/dist/vue.js';
+import VueRouter from 'vue-router';
+import city from "./components/city";
+import dashboard from "./components/dashboard";
 import searchRoom from "./components/searchRoom";
 import cityHotels from "./components/cityHotels";
 import passengerForm from './components/passengerform';
 import popularCities from "./components/popularCities";
 
+Vue.use(VueRouter)
+
 new Vue({
     el:'#app',
     components:{
         city,
+        dashboard,
         searchRoom,
         cityHotels,
         popularCities,
