8e79ff1134df7d4668a163bd68d3d8d2a279d375
Omid Reza <snip77.orh@gmail.com>
2021-09-26 03:58:00 +0330

    Fix some route problem

--- routes/operator.php
+++ routes/operator.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\Operator\RoomController;
 use App\Http\Controllers\Operator\HotelsController;
 
 Route::group(['prefix'=>'operator', 'middleware'=>['auth:api', 'role:operator']], function (){
+
+    Route::apiResource('hotels', HotelsController::class)->only('index');
+
     Route::group(['middleware'=>'operatorHasAccessHotel'],  function(){
-        Route::apiResource('hotels', HotelsController::class)->only('index', 'show');
+        Route::apiResource('hotels', HotelsController::class)->only('show');
         Route::group(['prefix'=>'hotels/{hotel}'], function (){
             Route::apiResource('rooms', RoomController::class)->only('store');
         });
